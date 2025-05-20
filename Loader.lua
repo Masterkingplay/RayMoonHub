6ab4a8015edf1f4f36be57c9af3b582dace1dd16
Masterkingplay <cristianricardogarciasaenz@gmail.com>
2025-05-20 02:04:12 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,7 @@ local Window = WindUI:CreateWindow({
     Icon = "zap",
     Author = "Script",
     Folder = "RayMoonHub",
-    Size = UDim2.fromOffset(580, 460),
+    Size = UDim2.fromOffset(540, 460),
     Transparent = true,
     Theme = "Dark",
     SideBarWidth = 200,
@@ -18,7 +18,7 @@ local Window = WindUI:CreateWindow({
         end,
     },
     KeySystem = {
-        Key = { "1234", "5678" },
+        Key = { "4G8H2K9D", "A1B3C5D7", "9P4M6J8L", "K3G9H2F4", "8N5B3V1X", "C7A9K4P2", "2M6J8G3H", "F4E8D2B6", "1L9N7P5R", "H8G4K2M9" },
         Note = "Key System.",
         Thumbnail = {
             Image = "rbxassetid://14205650077",
